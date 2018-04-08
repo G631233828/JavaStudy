44a454f99900cf9ab709caf1afa11bcc30e49c67
G631233828 <631233828@qq.com>
2018-04-08 22:43:38 +0800

    activiteMQ

--- GeneralSite/ActiveMQ/src/main/java/com/mq/test/Receiver.java
+++ GeneralSite/ActiveMQ/src/main/java/com/mq/test/Receiver.java
@@ -15,15 +15,16 @@ public class Receiver {
 	public static void main(String[] args) throws JMSException {
 
 		// 第一步，建立ConnectionFactory工厂对象，需要填入用户名，密码，以及链接地址，均使用默认即可，默认端口为：tcp://localhost:61616
-		ConnectionFactory connectionFactory = new ActiveMQConnectionFactory("fliay",
-				"123456", "tcp://localhost:61616");
+		ConnectionFactory connectionFactory = new ActiveMQConnectionFactory("admin",
+				"admin", "tcp://localhost:61616");
 
 		// 第二步，通过ConnectionFactory工程对象我们创建一个Connection连接，并且调用Connection的start方法开启链接，Connection默认是关闭的
 		Connection connection = connectionFactory.createConnection();
 		connection.start();// 开启
 
 		// 第三步，通过connection对象创建session会话（上下文环境对象），用于接收消息，参数配置1为是否启用事务，参数配置2为签收模式，一般设置自动签收
-		Session session = connection.createSession(Boolean.FALSE, Session.AUTO_ACKNOWLEDGE);
+		Session session = connection.createSession(Boolean.FALSE, Session.AUTO_ACKNOWLEDGE);//自动签收模式
+		//Session session = connection.createSession(Boolean.FALSE, Session.CLIENT_ACKNOWLEDGE);//客户端必须签收
 
 		//第四步，通过Session创建Destination对象，指的是一个客户端用来指定生产消息目标和消费消息来源的对象，在PTP模式中，Destination被称作Queue即队列
 		Destination destination = session.createQueue("first");
@@ -34,6 +35,9 @@ public class Receiver {
 		while(true){
 			
 			TextMessage msg = (TextMessage) consumer.receive();
+			
+			//msg.acknowledge();//手动签收 在Session.CLIENT_ACKNOWLEDGE模式下必须手动签收一下
+			
 			System.out.println("消费数据："+msg.getText());
 		}
 		
--- GeneralSite/ActiveMQ/src/main/java/com/mq/test/Sender.java
+++ GeneralSite/ActiveMQ/src/main/java/com/mq/test/Sender.java
@@ -16,30 +16,38 @@ import org.apache.activemq.ActiveMQConnectionFactory;
 public class Sender {
 	public static void main(String[] args) throws JMSException, InterruptedException {
 		// 第一步，建立ConnectionFactory工厂对象，需要填入用户名，密码，以及要连接的地址，均使用默认的即可，默认端口：“tcp://localhost:616161”
-		ConnectionFactory connectionFactory = new ActiveMQConnectionFactory("fliay",
-				"123456", "tcp://localhost:61616");
+		ConnectionFactory connectionFactory = new ActiveMQConnectionFactory("admin",
+				"admin", "tcp://localhost:61616");
 		// 第二步，简历ConnectionFactory工厂对象我们创建一个connection链接，并且调用Connection的start方法开启链接，Connection默认是关闭的
 		Connection connection = connectionFactory.createConnection();
 		connection.start();
 		
 		//第三步，通过connection对象创建session会话（上下文环境对象），用于接受消息，参数配置1为是否启用事务，参数配置2为签收模式，一般我们设置自动签收
-		Session session = connection.createSession(Boolean.FALSE, Session.AUTO_ACKNOWLEDGE);
+		Session session = connection.createSession(Boolean.TRUE, Session.AUTO_ACKNOWLEDGE);//自动签收模式 Boolean.TRUE 为支持事务提交
+		//Session session = connection.createSession(Boolean.TRUE, Session.CLIENT_ACKNOWLEDGE);//客户端必须签收
+		
 		
 		//第四步，通过Session 创建destination对象，指的是客户端用来指定生产消息目标和消费消息来源的对象，在PTP模式中，Destination被称作
-		Destination desination = session.createQueue("first");
+		Destination destination = session.createQueue("first");
 		
 		//第五步，我们需要通过Session对象创建消息的发送和接收对象（生产者和消费者）MessageProducer/MessageConsumer
 		MessageProducer producer = session.createProducer(null);
 		
 		//第六步，我们可以使用MessageProducer的setDeliveryMode方法为其设置持久化特性和非持久化特性（DeliveryMode）
-		producer.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
-		
+		//producer.setDeliveryMode(DeliveryMode.NON_PERSISTENT);//非持久化
+        producer.setDeliveryMode(DeliveryMode.PERSISTENT);
+
 		//第七步，最后我们使用JMS规范的TextMessage形式创建数据（通过Session对象），并用MessageProduce r的send方法发送数据，同理客户端用receive方法获取数据
-		for(int i =0;i<100;i++){
+		for(int i =1;i<10;i++){
 			TextMessage msg = session.createTextMessage("我是消息内容"+i);
-			producer.send(desination,msg);
-			TimeUnit.SECONDS.sleep(1);
+			//producer.send(destination,msg);
+			  producer.send(destination, msg, DeliveryMode.PERSISTENT, i, 1000*20);//i是优先级，20000存活时间
 		}
+		TimeUnit.SECONDS.sleep(3);
+		
+		//提交事务，整体提交数据
+		session.commit();
+		
 		
 		if(connection!=null){
 			connection.close();
