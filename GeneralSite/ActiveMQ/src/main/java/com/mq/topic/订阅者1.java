1e3a4df37d5245e83a214c9bf0dfb38f9cae061a
G631233828 <631233828@qq.com>
2018-06-11 09:46:26 +0800

    1

--- GeneralSite/ActiveMQ/src/main/java/com/mq/topic/订阅者1.java
+++ GeneralSite/ActiveMQ/src/main/java/com/mq/topic/订阅者1.java
@@ -7,7 +7,6 @@ import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.MessageConsumer;
 import javax.jms.MessageListener;
-import javax.jms.MessageProducer;
 import javax.jms.Session;
 import javax.jms.TextMessage;
 
