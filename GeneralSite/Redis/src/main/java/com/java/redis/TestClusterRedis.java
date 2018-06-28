e8c962b5a85050e7fa3e3ef3b7e725f866963061
G631233828 <631233828@qq.com>
2018-06-28 17:25:30 +0800

    redis 集群环境搭建

---
+++ GeneralSite/Redis/src/main/java/com/java/redis/TestClusterRedis.java
@@ -0,0 +1,37 @@
+package com.java.redis;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import redis.clients.jedis.HostAndPort;
+import redis.clients.jedis.JedisCluster;
+import redis.clients.jedis.JedisPoolConfig;
+
+
+public class TestClusterRedis {
+	
+	public static void main(String[] args) {
+		
+		Set<HostAndPort> jedisClusterNode = new HashSet<HostAndPort>();
+		jedisClusterNode.add(new HostAndPort("192.168.1.149", 7001));
+		jedisClusterNode.add(new HostAndPort("192.168.1.149", 7002));
+		jedisClusterNode.add(new HostAndPort("192.168.1.149", 7003));
+		jedisClusterNode.add(new HostAndPort("192.168.1.149", 7004));
+		jedisClusterNode.add(new HostAndPort("192.168.1.149", 7005));
+		jedisClusterNode.add(new HostAndPort("192.168.1.149", 7006));
+		
+		JedisPoolConfig cfg = new JedisPoolConfig();
+		cfg.setMaxTotal(100);
+		cfg.setMaxIdle(20);
+		cfg.setMaxWaitMillis(-1);
+		cfg.setTestOnBorrow(true);
+		JedisCluster jc = new JedisCluster(jedisClusterNode,6000,100,cfg);
+		
+		System.out.println(jc.set("age", "20"));
+		System.out.println(jc.get("age"));
+		
+	}
+	
+	
+
+}
